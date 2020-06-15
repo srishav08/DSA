6702bbfcf663d3d8bf9c0f1297cf4b0ab91a486f
srishav08 <singhrishav08@gmail.com>
2020-06-15 13:03:51 +0530

    2nd commit comments added

--- array-rotation/rotation_swap.js
+++ array-rotation/rotation_swap.js
@@ -1,5 +1,8 @@
 var arr = [1, 2, 3, 4, 5];
-for (j = 0; j < 3; j++) {
+var d = 3;
+//outer loop to rotate the elemets d times
+for (j = 0; j < d; j++) {
+  //rotate the array once by continuous swapping with the last element
   for (var i = 0; i < arr.length; i++) {
     var temp = arr[i];
     arr[i] = arr[arr.length - 1];
