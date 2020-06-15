aa1ce3688b4e5553bf1aba3c3a451edd92b525b4
srishav08 <singhrishav08@gmail.com>
2020-06-15 15:56:13 +0530

    search-in-rotated-and-sorted-array.js pivot search updated

--- array-rotation/search-in-rotated-and-sorted-array.js
+++ array-rotation/search-in-rotated-and-sorted-array.js
@@ -5,11 +5,31 @@ console.log(pivot);
 var index = find(arr, pivot, key);
 console.log(index);
 function arrPivot(arr) {
+  /**Linear search for pivot
+  
+  var start = 0;
+  var end = arr.length - 1;
   var index = 0;
   while (arr[index] < arr[index + 1]) {
     index++;
   }
   return index + 1;
+  
+  **/
+
+  //  binary search for pivot
+  while (start <= end) {
+    var mid = ~~((end + start) / 2);
+    console.log("this is md" + mid);
+    if (arr[mid] > arr[mid + 1]) {
+      return mid + 1;
+    } else if (arr[mid] > arr[start]) {
+      start = mid + 1;
+    } else {
+      end = mid - 1;
+    }
+  }
+  return 0;
 }
 function find(arr, pivot, key) {
   console.log(pivot);
